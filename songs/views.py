04bf7b0adf7c9b8211a400455c302da468bae4b4
YgorRupp <ygor.rupp@hotmail.com>
2023-04-27 14:34:33 -0300

    feat: finish deploy repo

---
+++ songs/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView, Response, status
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Song
+from rest_framework.pagination import PageNumberPagination
+from .serializers import SongSerializer
+from albums.models import Album
+
+from rest_framework import generics
+
+
+class SongView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Song.objects.all()
+
+        return Song.objects.filter(album_id=self.kwargs["pk"])
+
+    def perform_create(self, serializer) -> None:
+        serializer.save(album_id=self.kwargs["pk"])
---
+++ users/views.py
@@ -0,0 +1,19 @@
+from rest_framework.views import APIView, Request, Response, status
+from .models import User
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializers import UserSerializer
+from django.shortcuts import get_object_or_404
+from .permissions import IsAccountOwner
+from rest_framework import generics
+
+
+class UserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
